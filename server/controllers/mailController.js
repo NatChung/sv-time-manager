09bde6546d9948e589bb1293f74985e655cc564f
Nat <nat@starvedia.com>
2017-11-12 15:48:28 +0800

    [A] Added sent mail process

--- server/controllers/mailController.js
+++ server/controllers/mailController.js
@@ -1,3 +1,6 @@
+import email from 'emailjs/email'
+import { log } from 'util';
+import config from './config.json'
 
 export default class MailController{
     constructor({req, res}){
@@ -5,8 +8,33 @@ export default class MailController{
         this._res = res
     }
 
-    execute(){
+    content(){
+        let replyContent = ""
+        Object.keys(this._req.body).map((name, i) => {
+            replyContent += `${name}:\n`
+            this._req.body[name].done.map((_done, i) => {
+                replyContent += `  ${i}.${_done.item}\n`
+            })
+            replyContent += `\n`
+        })
 
-        this._res.status(200).json({response:"OK"})
+        return replyContent
+    }
+
+    today(){
+       return( new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''))
+    }
+
+    execute(){
+        email.server.connect(config)
+         .send({
+            text:    this.content(), 
+            from:    "Nat <nat@starvedia.com>", 
+            to:      "NatCung <nat.chung1@gmail.com>",
+            subject: this.today()+" RD daily"
+         }, (err, message) => {
+             if(err) throw err
+             this._res.status(200).json({response:"OK"})
+         })
     }
 }
